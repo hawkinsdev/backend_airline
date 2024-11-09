52ff0ac32deac36096d9701dd21f7365a2e55b47
Jesus Manuel Varela Miranda <57116065+hawkinsdev@users.noreply.github.com>
2024-11-09 02:20:00 -0500

    fix: eliminando caso que no era necesario ya que la información ya se tiene bien asociada.

--- config/data/itineraries.py
+++
@@ -1,6 +0,0 @@
-ITINERARIES = [
-  {"origin": 1, "destination": 6, "route": '["BOG", "MDE", "CTG"]', "total_duration": 130},
-  {"origin": 1, "destination": 7, "route": '["BOG", "CLO"]', "total_duration": 75},
-  {"origin": 2, "destination": 4, "route": '["BAQ", "SMR"]', "total_duration": 45},
-  {"origin": 1, "destination": 8, "route": '["BOG", "EOH"]', "total_duration": 55}
-]
